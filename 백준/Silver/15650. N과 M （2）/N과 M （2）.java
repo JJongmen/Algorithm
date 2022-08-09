8f59dc9dfa92802f56f55730d3877049cfc6a868
JJongmen <55042337+JJongmen@users.noreply.github.com>
2022-08-09 16:21:21 +0900

    [Silver III] Title: N과 M (2), Time: 168 ms, Memory: 16320 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static void recur(String comb) throws IOException {
+		if (comb.length() == M * 2) {
+			bw.write(comb + "\n");
+			return;
+		}
+		char last = '0';
+		int len = comb.length();
+		if (len > 0) {
+			last = comb.charAt(len - 2);
+		}
+		for (int i = last - '0' + 1; i <= N; i++) {
+			recur(comb + i + " ");
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	    
+		recur("");
+		
+	    bw.flush();
+	    br.close();
+	    bw.close();
+	}
+}
