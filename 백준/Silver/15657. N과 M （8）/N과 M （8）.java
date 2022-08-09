2fe1367400a94182749ad7e847b91f93a4733af1
JJongmen <55042337+JJongmen@users.noreply.github.com>
2022-08-09 16:58:27 +0900

    [Silver III] Title: N과 M (8), Time: 260 ms, Memory: 19628 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] nums;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static void recur(String comb, int last) throws IOException {
+		if (comb.length() == M) {
+			for (int i = 0; i < M; i++) {
+				bw.write(nums[comb.charAt(i) - '0'] + " ");
+			}
+			bw.write("\n");
+		} else {
+			for (int i = last; i < N; i++) {
+				recur(comb + i, i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+	
+		recur("", 0);
+		
+	    bw.flush();
+	    br.close();
+	    bw.close();
+	}
+}
