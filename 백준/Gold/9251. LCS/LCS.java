3823ae0d15ab5eabfff418f3808f2b5cd0b73ede
JJongmen <55042337+JJongmen@users.noreply.github.com>
2022-09-06 14:21:56 +0900

    [Gold V] Title: LCS, Time: 252 ms, Memory: 21572 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+
+        int[][] memo = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    memo[i][j] = memo[i - 1][j - 1] + 1;
+                } else {
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(memo[str1.length()][str2.length()]);
+    }
+}
