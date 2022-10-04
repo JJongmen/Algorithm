d8560060e4dd8b15bad6282d61106bf8577bf143
JJongmen <55042337+JJongmen@users.noreply.github.com>
2022-10-05 00:49:48 +0900

    [Gold II] Title: 2048 (Easy), Time: 404 ms, Memory: 93008 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -4,42 +4,24 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    private static int[][] original;
     private static int[][] board;
-    private static int max = 2;
     private static int N;
 
-    private static void func(int cur) {
-        if (cur == 5) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    max = Math.max(max, board[i][j]);
-                }
-            }
-            return;
-        }
-        int[][] tmp = deepCopy(board);
-        moveLeft();
-        func(cur + 1);
-        board = deepCopy(tmp);
-        moveRight();
-        func(cur + 1);
-        board = deepCopy(tmp);
-        moveUp();
-        func(cur + 1);
-        board = deepCopy(tmp);
-        moveDown();
-        func(cur + 1);
-    }
-
-    private static int[][] deepCopy(int[][] original) {
+    private static void rotate() {
         int[][] tmp = new int[N][N];
         for (int i = 0; i < N; i++) {
-            tmp[i] = original[i].clone();
+            tmp[i] = board[i].clone();
         }
-        return tmp;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                board[i][j] = tmp[j][N - 1 - i];
+            }
+        }
     }
 
-    private static void moveLeft() {
+    private static void move(int direction) {
+        while (direction-- > 0) rotate();
         for (int i = 0; i < N; i++) {
             Deque<Integer> deque = new LinkedList<>();
             boolean combineFlag = false;
@@ -55,81 +37,6 @@ public class Main {
                 combineFlag = false;
             }
             for (int j = 0; j < N; j++) {
-                if (!deque.isEmpty()) {
-                    board[i][j] = deque.poll();
-                    continue;
-                }
-                board[i][j] = 0;
-            }
-        }
-    }
-
-    private static void moveUp() {
-        for (int j = 0; j < N; j++) {
-            Deque<Integer> deque = new LinkedList<>();
-            boolean combineFlag = false;
-            for (int i = 0; i < N; i++) {
-                if (board[i][j] == 0) continue;
-                if (!deque.isEmpty() && deque.peekLast() == board[i][j] && !combineFlag) {
-                    deque.pollLast();
-                    deque.offer(board[i][j] * 2);
-                    combineFlag = true;
-                    continue;
-                }
-                deque.offer(board[i][j]);
-                combineFlag = false;
-            }
-            for (int i = 0; i < N; i++) {
-                if (!deque.isEmpty()) {
-                    board[i][j] = deque.poll();
-                    continue;
-                }
-                board[i][j] = 0;
-            }
-        }
-    }
-
-    private static void moveDown() {
-        for (int j = 0; j < N; j++) {
-            Deque<Integer> deque = new LinkedList<>();
-            boolean combineFlag = false;
-            for (int i = N - 1; i >= 0; i--) {
-                if (board[i][j] == 0) continue;
-                if (!deque.isEmpty() && deque.peekLast() == board[i][j] && !combineFlag) {
-                    deque.pollLast();
-                    deque.offer(board[i][j] * 2);
-                    combineFlag = true;
-                    continue;
-                }
-                deque.offer(board[i][j]);
-                combineFlag = false;
-            }
-            for (int i = N - 1; i >= 0; i--) {
-                if (!deque.isEmpty()) {
-                    board[i][j] = deque.poll();
-                    continue;
-                }
-                board[i][j] = 0;
-            }
-        }
-    }
-
-    private static void moveRight() {
-        for (int i = 0; i < N; i++) {
-            Deque<Integer> deque = new LinkedList<>();
-            boolean combineFlag = false;
-            for (int j = N - 1; j >= 0; j--) {
-                if (board[i][j] == 0) continue;
-                if (!deque.isEmpty() && deque.peekLast() == board[i][j] && !combineFlag) {
-                    deque.pollLast();
-                    deque.offer(board[i][j] * 2);
-                    combineFlag = true;
-                    continue;
-                }
-                deque.offer(board[i][j]);
-                combineFlag = false;
-            }
-            for (int j = N - 1; j >= 0; j--) {
                 if (!deque.isEmpty()) {
                     board[i][j] = deque.poll();
                     continue;
@@ -142,24 +49,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        original = new int[N][N];
         board = new int[N][N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+                original[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        func(0);
+        int max = 2;
+        for (int tmp = 0; tmp < 1024; tmp++) {
+            for (int i = 0; i < N; i++) {
+                board[i] = original[i].clone();
+            }
+            int dir = tmp;
+            for (int i = 0; i < 5; i++) {
+                move(dir % 4);
+                dir /= 4;
+            }
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    max = Math.max(max, board[i][j]);
+                }
+            }
+        }
         System.out.println(max);
-    }
-
-    private static void printBoard() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.printf("%5d", board[i][j]);
-            }
-            System.out.println();
-        }
     }
 }
