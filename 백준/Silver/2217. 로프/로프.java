4b3b5292caa02132d26cdd62f2bac7a48d0f4cd7
JJongmen <55042337+JJongmen@users.noreply.github.com>
2022-10-09 11:06:50 +0900

    [Silver IV] Title: 로프, Time: 336 ms, Memory: 25436 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,15 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int[] ropes = new int[100005];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] ropes = new int[N];
         for (int i = 0; i < N; i++) {
-            ropes[i] = scan.nextInt();
+            ropes[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(ropes, 0, N);
+        Arrays.sort(ropes);
 
         int result = 0;
         // 로프를 i개 사용할 때의 들어올릴 수 있는 물체의 최대 중량
