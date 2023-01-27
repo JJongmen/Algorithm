65e256e5026c98bb3fecb400e284db2b5148332e
JJongmen <55042337+JJongmen@users.noreply.github.com>
2023-01-27 11:10:19 +0900

    [Gold III] Title: 오등큰수, Time: 1008 ms, Memory: 142000 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final int MAX = 1000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] cnts = new int[MAX + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            cnts[arr[i]]++;
+        }
+
+        int[] result = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && cnts[arr[stack.peek()]] < cnts[arr[i]]) {
+                result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
