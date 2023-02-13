f3c65cb5d97383e3b6c653c6db18a848be7cc70b
JJongmen <55042337+JJongmen@users.noreply.github.com>
2023-02-13 16:25:34 +0900

    [Gold IV] Title: 검문, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.java
+++ 백준/Gold/2981. 검문/검문.java
@@ -1,12 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 	
 	static int gcd(int a, int b) {
-		if (a % b == 0) {
-			return b;
+		if (b == 0) {
+			return a;
 		}
 		return gcd(b, a % b);
 	}
@@ -20,16 +24,28 @@ public class Main {
 			paper[i] = Integer.parseInt(br.readLine());
 		}
 
-		int gcdTmp = Math.abs(paper[1] - paper[0]);
+		Arrays.sort(paper);
+		int gcdVal = paper[1] - paper[0];
+
 		for (int i = 2; i < N; i++) {
-			gcdTmp = gcd(gcdTmp, Math.abs(paper[i] - paper[i - 1]));
+			gcdVal = gcd(gcdVal, paper[i] - paper[i - 1]);
 		}
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 2; i <= gcdTmp; i++) {
-			if (gcdTmp % i != 0) continue;
-			sb.append(i).append(" ");
+		List<Integer> result = new ArrayList<>();
+		for (int i = 2; i * i <= gcdVal; i++) {
+			if (gcdVal % i != 0) continue;
+			result.add(i);
+			if (i * i != gcdVal) {
+				result.add(gcdVal / i);
+			}
 		}
+		Collections.sort(result);
+
+		StringBuilder sb = new StringBuilder();
+		for (Integer val : result) {
+			sb.append(val).append(" ");
+		}
+		sb.append(gcdVal);
 		System.out.println(sb);
 	}
 }
