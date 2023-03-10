c373e9115b8c993171ea6604115b3bfb6a97a676
JJongmen <55042337+JJongmen@users.noreply.github.com>
2023-03-10 22:33:55 +0900

    Add files via upload

---
+++ 백준/Gold/Q1188.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1188 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        System.out.println(M - gcd(N, M));
+    }
+
+    private static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+}
