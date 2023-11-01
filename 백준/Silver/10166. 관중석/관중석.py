4bb3a803254af289d878e420a6c1fe8cd8ae1aa3
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-11-01 10:31:52 +0900

    [Silver I] Title: 관중석, Time: 536 ms, Memory: 63908 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,20 +1,21 @@
 import sys
+from math import gcd
 
-def gcd(a, b):
-    if b == 0:
-        return a
-    return gcd(b, a % b)
+def solve():
+    input = sys.stdin.readline
 
-input = sys.stdin.readline
+    D1, D2 = map(int, input().rstrip().split())
 
-D1, D2 = map(int, input().rstrip().split())
+    memo = [[False] * 2001 for _ in range(2001)]
+    cnt = 0
+    for i in range(D1, D2 + 1):
+        for j in range(1, i + 1):
+            gcd_ = gcd(i, j)
+            x, y = i // gcd_, j // gcd_
+            if memo[x][y]: continue
+            memo[x][y] = True
+            cnt += 1
+    return cnt
 
-memo = [[False] * 2001 for _ in range(2001)]
-cnt = 0
-for i in range(D1, D2 + 1):
-    for j in range(1, i + 1):
-        gcd_ = gcd(i, j)
-        if memo[i // gcd_][j // gcd_]: continue
-        memo[i // gcd_][j // gcd_] = True
-        cnt += 1
-print(cnt)
+
+print(solve())
