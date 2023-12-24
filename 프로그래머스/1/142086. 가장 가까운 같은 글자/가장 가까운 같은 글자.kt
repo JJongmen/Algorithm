ac2968d7df92ce1cd1557b7b576c35dfbcc67405
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-12-24 16:31:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 40.91 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
@@ -1,11 +1,9 @@
 class Solution {
     fun solution(s: String): IntArray {
-        var answer: IntArray = intArrayOf()
-        var lastIndices: IntArray = IntArray(26) { -1 }
-        s.forEachIndexed { index, ch ->
-            answer += if (lastIndices[ch - 'a'] == -1) -1 else index - lastIndices[ch - 'a']
-            lastIndices[ch - 'a'] = index
-        }
-        return answer
+        return s.withIndex().map { (index, ch) -> 
+            s.substring(0 until index).lastIndexOf(ch).let {
+                if (it == -1) -1 else index - it
+            }
+        }.toIntArray()
     }
 }
