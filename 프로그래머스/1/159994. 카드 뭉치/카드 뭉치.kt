0f6e11806e9950cdf17894a8b913eb5156485e56
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-12-24 18:03:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -2,16 +2,10 @@ class Solution {
     fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
         var idx1 = 0
         var idx2 = 0
-        for (word in goal) {
-            if (idx1 <= cards1.lastIndex && cards1[idx1] == word) {
-                idx1 += 1
-                continue
-            }
-            if (idx2 <= cards2.lastIndex && cards2[idx2] == word) {
-                idx2 += 1
-                continue
-            }
-            return "No"
+        goal.forEach {
+            if (idx1 < cards1.size && cards1[idx1] == it) idx1++
+            else if (idx2 < cards2.size && cards2[idx2] == it) idx2++
+            else return "No"
         }
         return "Yes"
     }
