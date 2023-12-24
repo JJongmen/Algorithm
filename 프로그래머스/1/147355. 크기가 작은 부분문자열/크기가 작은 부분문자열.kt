7df3bfad1ec8267d26e4ac43a046e14a23b0adc6
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-12-24 15:41:44 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 13.65 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,10 +1,6 @@
 class Solution {
-    fun solution(t: String, p: String): Int {
-        var answer: Int = 0
-        for (i in 0..(t.length - p.length)) {
-            if (t.substring(i until i + p.length).compareTo(p) > 0) continue
-            answer += 1
-        }
-        return answer
-    }
+    fun solution(t: String, p: String) =
+        (0..t.length - p.length)
+        .map { t.substring(it until it + p.length) }
+        .count { it <= p }
 }
