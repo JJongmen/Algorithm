16530afadcc7cbbe33c995266e6f0e5a75ebb3cc
박종윤 <55042337+JJongmen@users.noreply.github.com>
2023-12-27 23:33:59 +0900

    [level 1] Title: 둘만의 암호, Time: 22.38 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,18 +1,8 @@
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
-        var answer = ""
-        s.forEach { ch ->
-            var cnt = 0
-            var tmp = ch
-            while (cnt < index) {
-                while (true) {
-                    if (tmp++ == 'z') tmp = 'a'
-                    if (!skip.contains(tmp)) break
-                }
-                cnt++
-            }
-            answer += tmp
-        }
-        return answer
+        val alphabets = ('a'..'z').filter { it !in skip }
+        return s.toList().map {
+            alphabets[(alphabets.indexOf(it) + index) % alphabets.size]
+        }.joinToString("")
     }
 }
