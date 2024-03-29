5e6e7e44d03395beb62f1d300acbefa3ca1dda81
박종윤 <55042337+JJongmen@users.noreply.github.com>
2024-03-30 02:35:22 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 70.11 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -5,9 +5,6 @@ def solution(arr):
         for i in range(x, x + L):
             for j in range(y, y + L):
                 if arr[i][j] == target: continue
-                result = [i + j for i, j in zip(result, compressure(x, y, L // 2))]
-                result = [i + j for i, j in zip(result, compressure(x, y + L // 2, L // 2))]
-                result = [i + j for i, j in zip(result, compressure(x + L // 2, y, L // 2))]
-                return [i + j for i, j in zip(result, compressure(x + L // 2, y + L // 2, L // 2))]
+                return [sum(values) for values in zip(compressure(x, y, L // 2), compressure(x, y + L // 2, L // 2), compressure(x + L // 2, y, L // 2), compressure(x + L // 2, y + L // 2, L // 2))]
         return [1, 0] if target == 0 else [0, 1]
     return compressure(0, 0, len(arr))
